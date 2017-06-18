cf1956094a4c3845040d31c17dd2336dd919fed3
JorisPalings <joris.palings@student.ucll.be>
2017-06-18 17:49:48 +0200

    Implemented .txt parsing strategy

--- js/strategies/txt.js
+++ js/strategies/txt.js
@@ -1,4 +1,36 @@
+'use strict'
+
+function Record(date, time, frequency, mode, myCall, sentRST, myExchange, 
+  myGridSquare, call, receivedRST, exchange, gridSquare) {
+    this.date = date
+    this.time = time
+    this.frequency = frequency
+    this.mode = mode
+    this.myCall = myCall
+    this.sentRST = sentRST
+    this.myExchange = myExchange
+    this.myGridSquare = myGridSquare
+    this.call = call
+    this.receivedRST = receivedRST
+    this.exchange = exchange
+    this.gridSquare = gridSquare
+  }
+
 const parse = function(file, fileContents) {
+  // Split the file's contents on newlines
+  let records = fileContents.trim().split(/\n/g)
+
+  // If the first mine of the file's contents doesn't start with a number (date),
+  // remove it
+  if(!records[0].charAt(0).match(/[0-9]1/g)) {
+    records.shift()
+  }
+
+  // Split every record int ofields
+  records.forEach((record, recordIndex) => {
+    records[recordIndex] = new Record(...(record.trim().split(/\s+/g)))
+  })
+  console.log(records)
 }
 
 module.exports.parse = parse
