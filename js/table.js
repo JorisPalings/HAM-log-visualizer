36634818076b897e13310662bfa20636e26f30c2
JorisPalings <joris.palings@student.ucll.be>
2017-06-21 03:21:36 +0200

    Added getRecords, removed logging

--- js/table.js
+++ js/table.js
@@ -2,18 +2,16 @@ angular.module('tableApp', [])
   .controller('TableController', ['$scope', $scope => {
     $scope.records = [];
 
+    $scope.getRecords = () => {
+      return $scope.records;
+    }
+
     $scope.addRecords = (records) => {
-      console.log('entered addRecords');
-      console.log('current records', $scope.records);
-      console.log('new records', records);
       $scope.records = [...$scope.records, ...records];
-      console.log('current + new records', $scope.records);
       $scope.$apply();
     };
 
     $scope.clear = () => {
-      console.log('entered clear');
       $scope.records = [];
-      console.log('cleared records', $scope.records);
     }
   }]);
