11bfadb5a6e91c498c13613f83d38ffc45e14357
JorisPalings <joris.palings@student.ucll.be>
2017-06-25 01:06:13 +0200

    Added & implemented (sticky) search bar, implemented file removal

--- js/table.js
+++ js/table.js
@@ -3,24 +3,42 @@ angular.module('tableApp', [])
     $scope.files = [];
     $scope.records = [];
 
+    // Return the names of all loaded files
     $scope.getFiles = () => {
       return $scope.files;
     }
 
+    // Return all records in all loaded files
     $scope.getRecords = () => {
       return $scope.records;
     }
 
-    $scope.addFile = (file) => {
+    // Add a file's name to the list of files
+    $scope.addFile = file => {
       $scope.files.push(file);
       $scope.$apply();
-    };
+    }
 
-    $scope.addRecords = (records) => {
+    // Delete a file from the list of files and
+    // its records from the list of records
+    $scope.deleteFile = file => {
+      $scope.files.splice($scope.files.indexOf(file), 1);
+      $scope.deleteRecords(file);
+    }
+
+    // Add records to the list of records
+    $scope.addRecords = records => {
       $scope.records = [...$scope.records, ...records];
       $scope.$apply();
-    };
+    }
 
+    // Delete the records from the given file
+    // from the list of records
+    $scope.deleteRecords = file => {
+      $scope.records = $scope.records.filter(record => record.fileName != file);
+    }
+
+    // Clear all files, as well as all records
     $scope.clear = () => {
       $scope.files = [];
       $scope.records = [];
